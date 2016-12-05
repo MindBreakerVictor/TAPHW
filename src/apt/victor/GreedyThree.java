b5adcd02812daad1e3ede9a29917bd84fa0748e6
MindBreakerVictor <victor611@yahoo.com>
2016-12-05 16:04:52 +0200

    Code refactoring.

---
+++ src/apt/victor/GreedyThree.java
@@ -0,0 +1,111 @@
+package apt.victor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class GreedyThree {
+
+    private static class Activity implements Comparable<Activity> {
+        int index;
+        int profit;
+        int deadline;
+
+        Activity(int index, int profit, int deadline) {
+            this.index = index;
+            this.profit = profit;
+            this.deadline = deadline;
+        }
+
+        @Override
+        public int compareTo(Activity right) {
+            if (deadline > right.deadline)
+                return -1;
+
+            if (deadline < right.deadline)
+                return 1;
+
+            if (deadline == right.deadline) {
+                if (profit > right.profit)
+                    return -1;
+
+                if (profit < right.profit)
+                    return 1;
+            }
+
+            return Integer.compare(index, right.index);
+        }
+    }
+
+    private static class ActivityComparator implements Comparator<Activity> {
+        public int compare(Activity left, Activity right) {
+            return right.profit - left.profit;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("data.in"));
+            PrintWriter writer = new PrintWriter("data.out");
+            TreeSet<Activity> activities = new TreeSet<>();
+
+            if (!scanner.hasNextInt())
+                return;
+
+            int index = 0;
+            int activitiesNo = scanner.nextInt();
+
+            for (int i = 0; i < activitiesNo; ++i) {
+                if (!scanner.hasNextInt()) {
+                    System.out.println("There are less activities then specified. INVALID DATA!");
+                    return;
+                }
+
+                int profit = scanner.nextInt();
+
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Activity without deadline. INVALID DATA!");
+                    return;
+                }
+
+                activities.add(new Activity(++index, profit, scanner.nextInt()));
+            }
+
+            int profit = 0;
+            int currentDeadline = activities.first().deadline;
+            PriorityQueue<Activity> jobs = new PriorityQueue<>(activitiesNo, new ActivityComparator());
+            Stack<Activity> todo = new Stack<>();
+
+            while ((!activities.isEmpty() || !jobs.isEmpty()) && currentDeadline > 0) {
+                while (!activities.isEmpty() && activities.first().deadline == currentDeadline)
+                    jobs.add(activities.pollFirst());
+
+                --currentDeadline;
+
+                if (jobs.isEmpty()) {
+                    profit += activities.first().profit;
+                    todo.push(activities.pollFirst());
+                    continue;
+                }
+
+                profit += jobs.peek().profit;
+                todo.push(jobs.poll());
+            }
+
+            writer.println(profit);
+
+            Activity[] act = todo.toArray(new Activity[jobs.size()]);
+
+            for (int i = act.length - 1; i >= 0; --i)
+                writer.printf("%d ", act[i].index);
+
+            writer.close();
+            scanner.close();
+        }
+        catch (FileNotFoundException excep) {
+            System.out.println(excep.getMessage());
+        }
+    }
+}
+
---
+++ src/apt/victor/GreedyTwo.java
@@ -0,0 +1,148 @@
+package apt.victor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class GreedyTwo {
+
+    private static class Text implements Comparable<Text> {
+        int index;
+        int length;
+        int freq;
+
+        Text(int index, int length, int freq) {
+            this.index = index;
+            this.length = length;
+            this.freq = freq;
+        }
+
+        @Override
+        public int compareTo(Text right) {
+            double dif = ((double)length / (double)freq) - ((double)right.length / (double)right.freq);
+
+            if (dif < 0)
+                return -1;
+
+            if (dif > 0)
+                return 1;
+
+            return 0;
+        }
+    }
+
+    private static class TextB implements Comparable<TextB> {
+        int index;
+        int length;
+        int bandId;
+
+        TextB(int index, int length) {
+            this.index = index;
+            this.length = length;
+            this.bandId = 0;
+        }
+
+        @Override
+        public int compareTo(TextB right) {
+            return length - right.length;
+        }
+    }
+
+    public static void mainA(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("data.in"));
+            PrintWriter writer = new PrintWriter("data.out");
+            TreeSet<Text> texts = new TreeSet<>();
+
+            if (!sc.hasNextInt())
+                return;
+
+            int index = 0;
+            int textsNo = sc.nextInt();
+
+            for (int i = 0; i < textsNo; ++i) {
+                if (!sc.hasNextInt()) {
+                    System.out.println("There are less texts than specified. INVALID DATA!");
+                    return;
+                }
+
+                int length = sc.nextInt();
+
+                if (!sc.hasNextInt()) {
+                    System.out.println("Text without frequency. INVALID DATA!");
+                    return;
+                }
+
+                int freq = sc.nextInt();
+                texts.add(new Text(++index, length, freq));
+            }
+
+            for (Text text : texts) writer.printf("%d ", text.index);
+
+            writer.close();
+            sc.close();
+        } catch (FileNotFoundException excep) {
+            System.out.println(excep.getMessage());
+        }
+    }
+
+    public static void mainB(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("data.in"));
+            PrintWriter writer = new PrintWriter("data.out");
+            TreeSet<TextB> texts = new TreeSet<>();
+
+            if (!sc.hasNextInt())
+                return;
+
+            int index = 0;
+            int textsNo = sc.nextInt();
+
+            if (!sc.hasNextInt())
+                return;
+
+            int bandsNo = sc.nextInt();
+
+            for (int i = 0; i < textsNo; ++i) {
+                if (!sc.hasNextInt()) {
+                    System.out.println("There are less texts than specified. INVALID DATA!");
+                    return;
+                }
+
+                int length = sc.nextInt();
+                texts.add(new TextB(++index, length));
+            }
+
+            TextB[] txts = texts.toArray(new TextB[texts.size()]);
+
+            for (int i = 0; i < txts.length; ++i) {
+                int band = (i + 1) % bandsNo;
+
+                txts[i].bandId = (band == 0) ? bandsNo : band;
+            }
+
+            int written = 0;
+            int currentBand = 1;
+
+            while (written < textsNo) {
+                for (TextB text : txts)
+                    if (text.bandId == currentBand) {
+                        writer.printf("%d ", text.index);
+                        ++written;
+                    }
+
+                writer.printf("\n");
+                ++currentBand;
+            }
+
+            writer.close();
+            sc.close();
+        }
+        catch (FileNotFoundException excep) {
+            System.out.println(excep.getMessage());
+        }
+    }
+}
+
